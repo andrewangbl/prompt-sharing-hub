ebddcb44232990ed914d9bfc21f5f7ecb6f9661d
andrewangbl <bw2537@nyu.edu>
2024-05-27 17:52:23 -0400

    supense typo fix

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {useState, useEffect, Suspence} from 'react';
+import {useState, useEffect, Suspense} from 'react';
 import {useRouter,useSearchParams} from 'next/navigation';
 
 import Form from '@components/Form';
